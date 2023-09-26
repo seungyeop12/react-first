84058252565dab88cf79959c070ac24c81d50b55
seungyeop12 <art7449@gmail.com>
2023-09-26 10:10:18 +0900

    useeffext가 사용되는 실제 사례

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -2,17 +2,25 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 
 function Popup() {
+	console.log('팝업재호출');
+
 	const [Num, setNum] = useState(0);
 	useEffect(() => {
 		console.log('popup생성');
+		console.log('무거운 유튜브 데이타 fetching');
 
 		return () => {
 			console.log('popup소멸');
 		};
 	}, []);
+
+	useEffect(() => {
+		console.log('popup의 Num 상태변화');
+	}, [Num]);
+
 	return (
 		<aside>
-			<h1>0</h1>
+			<h1>{Num}</h1>
 			<button onClick={() => setNum(Num - 1)}>minus</button>
 			<button onClick={() => setNum(Num + 1)}>plus</button>
 		</aside>

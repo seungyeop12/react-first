6718e6dd6a6cd704640ab142acb6b2006573aec8
seungyeop12 <art7449@gmail.com>
2023-09-26 09:46:09 +0900

    팝업 안 숫자 증가 및 감소

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,5 +1,22 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 function Popup() {
-	return <aside>Popup</aside>;
+	const [Num, setNum] = useState(0);
+	useEffect(() => {
+		console.log('popup생성');
+
+		return () => {
+			console.log('popup소멸');
+		};
+	}, []);
+	return (
+		<aside>
+			<h1>0</h1>
+			<button onClick={() => setNum(Num - 1)}>minus</button>
+			<button onClick={() => setNum(Num + 1)}>plus</button>
+		</aside>
+	);
 }
 
 export default Popup;
